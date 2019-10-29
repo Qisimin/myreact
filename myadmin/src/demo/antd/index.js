401515968b62b9f5610cf1cae84d5922586e1253
Qisimin <1726556836@qq.com>
2019-10-29 21:13:16 +0800

    zc init

---
+++ myadmin/src/demo/antd/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'antd'
+function Less() {
+  return (
+    <div className="less">
+      <Button>么么哒</Button>
+    </div>
+  );
+}
+export default Less;
--- myadmin/src/index.js
+++ myadmin/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import App from './demo/less/index';
+import App from './demo/antd';
+// import 'antd/dist/antd.css'
+//全局引入样式
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
